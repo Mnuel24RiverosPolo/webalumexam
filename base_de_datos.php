2ea4f496e2ce96a07aaa91e3ff7261a688dd7414
Manuel <mriverospolo@gmail.com>
2023-04-20 12:12:54 -0500

    version v2.1

--- base_de_datos.php
+++ base_de_datos.php
@@ -2,13 +2,14 @@
 
 $contraseña = "password=pchWOPmQqBlgDgKiN6i4";
 $usuario = "user=postgres";
-$nombreBaseDeDatos = "dbname=railway";
+$nombreBaseDeDatos = "railway";
 
-$rutaServidor = "host=containers-us-west-137.railway.app"; 
-$puerto = "port=6939"; 
+$rutaServidor = "containers-us-west-137.railway.app"; 
+$puerto = "6939"; 
 try { 
     $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
 echo "Ocurrié un error con la base de datos: " . $e->getMessage();
 }
+?>
